dec29060a950e633ac396203af83bfd1d045082f
Alisharai1 <alisharai1997@gmail.com>
2026-07-29 18:43:39 +0530

    liveCheck endpoint tested successfully

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
+app.get('/liveCheck', (_req, res) => {
+    res.send(200).json()
+})
 
 app.listen(PORT, (error) => {
     if (error) {

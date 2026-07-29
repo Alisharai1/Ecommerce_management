ec2fa86c495e7605a85cd7759c3f56563a1fefd0
Alisharai1 <alisharai1997@gmail.com>
2026-07-29 18:42:33 +0530

    initialized express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("installations done for ticket 1");
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(`application is up, running on ${PORT}`)
+})
